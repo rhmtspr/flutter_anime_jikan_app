5e17b8558de8c3f2e060f0598999eb1b388fc397
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-16 13:10:34 +0800

    add hive and favorite feature in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:anime_jikan_app/app.dart';
+import 'package:anime_jikan_app/core/di/injection.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'app.dart';
-import 'core/di/injection.dart';
+import 'features/anime/data/models/favorite_anime_model.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Hive.initFlutter();
+
+  // Register adapter
+  Hive.registerAdapter(FavoriteAnimeModelAdapter());
+
+  // Open box
+  await Hive.openBox<FavoriteAnimeModel>('favorites');
 
   await initDependencies();
 
