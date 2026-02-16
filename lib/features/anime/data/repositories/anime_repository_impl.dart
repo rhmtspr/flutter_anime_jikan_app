ba74f019807b00b4f73659c1d5e6243c94ff135b
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-16 12:56:47 +0800

    change the methods to return (failure, data)

--- lib/features/anime/data/repositories/anime_repository_impl.dart
+++ lib/features/anime/data/repositories/anime_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import '../../../../core/errors/failures.dart';
 import '../../domain/entities/anime.dart';
 import '../../domain/repositories/anime_repository.dart';
 import '../datasources/anime_remote_datasource.dart';
@@ -8,19 +10,47 @@ class AnimeRepositoryImpl implements AnimeRepository {
   AnimeRepositoryImpl(this.remote);
 
   @override
-  Future<List<Anime>> getTopAnime(int page) async {
-    final models = await remote.getTopAnime(page: page);
-    return models;
+  Future<(Failure?, List<Anime>?)> getTopAnime(int page) async {
+    try {
+      final models = await remote.getTopAnime(page: page);
+      return (null, models);
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.connectionError) {
+        return (const NetworkFailure(), null);
+      }
+      return (const ServerFailure(), null);
+    } catch (_) {
+      return (const UnknownFailure(), null);
+    }
   }
 
   @override
-  Future<List<Anime>> searchAnime(String query, int page) async {
-    final models = await remote.searchAnime(query: query, page: page);
-    return models;
+  Future<(Failure?, List<Anime>?)> searchAnime(String query, int page) async {
+    try {
+      final models = await remote.searchAnime(query: query, page: page);
+      return (null, models);
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.connectionError) {
+        return (const NetworkFailure(), null);
+      }
+      return (const ServerFailure(), null);
+    } catch (_) {
+      return (const UnknownFailure(), null);
+    }
   }
 
   @override
-  Future<Anime> getAnimeDetail(int id) async {
-    return await remote.getAnimeDetail(id);
+  Future<(Failure?, Anime?)> getAnimeDetail(int id) async {
+    try {
+      final model = await remote.getAnimeDetail(id);
+      return (null, model);
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.connectionError) {
+        return (const NetworkFailure(), null);
+      }
+      return (const ServerFailure(), null);
+    } catch (_) {
+      return (const UnknownFailure(), null);
+    }
   }
 }
