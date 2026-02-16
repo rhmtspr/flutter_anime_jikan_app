ec4bc4ed10bbf00686530d95548b3b3aae3a366f
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-16 12:56:06 +0800

    create failures.dart

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,16 @@
+abstract class Failure {
+  final String message;
+  const Failure(this.message);
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure([super.message = 'Server error occurred']);
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure([super.message = 'No internet connection']);
+}
+
+class UnknownFailure extends Failure {
+  const UnknownFailure([super.message = 'Unexpected error occurred']);
+}
