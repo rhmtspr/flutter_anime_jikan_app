4178ecb37279758deb6e72d13c6dc426b47b60f2
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-16 13:12:10 +0800

    update repository; add hive box, local datasuorce, favorite and favorite cubit

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -3,10 +3,15 @@ import 'package:anime_jikan_app/features/anime/data/repositories/anime_repositor
 import 'package:anime_jikan_app/features/anime/domain/repositories/anime_repository.dart';
 import 'package:get_it/get_it.dart';
 import '../network/dio_client.dart';
-
+import 'package:hive/hive.dart';
+import '../../features/anime/data/models/favorite_anime_model.dart';
+import '../../features/anime/data/datasources/anime_local_datasource.dart';
 import '../../features/anime/domain/usecases/get_top_anime.dart';
 import '../../features/anime/domain/usecases/search_anime.dart';
 import '../../features/anime/domain/usecases/get_anime_detail.dart';
+import '../../features/anime/domain/usecases/toggle_favorite.dart';
+import '../../features/anime/domain/usecases/get_favorites.dart';
+import '../../features/anime/presentation/bloc/favorites_cubit.dart';
 
 final sl = GetIt.instance;
 
@@ -21,10 +26,32 @@ Future<void> initDependencies() async {
   );
 
   // Repository
-  sl.registerLazySingleton<AnimeRepository>(() => AnimeRepositoryImpl(sl()));
+  sl.registerLazySingleton<AnimeRepository>(
+    () => AnimeRepositoryImpl(
+      sl(), // remote
+      sl(), // local
+    ),
+  );
 
   // Use cases
   sl.registerLazySingleton(() => GetTopAnime(sl()));
   sl.registerLazySingleton(() => SearchAnime(sl()));
   sl.registerLazySingleton(() => GetAnimeDetail(sl()));
+
+  // Hive box
+  sl.registerLazySingleton<Box<FavoriteAnimeModel>>(
+    () => Hive.box<FavoriteAnimeModel>('favorites'),
+  );
+
+  // Local datasource
+  sl.registerLazySingleton<AnimeLocalDataSource>(
+    () => AnimeLocalDataSourceImpl(sl()),
+  );
+
+  // Favorite
+  sl.registerLazySingleton(() => ToggleFavorite(sl()));
+  sl.registerLazySingleton(() => GetFavorites(sl()));
+
+  // Favorite Cubit
+  sl.registerFactory(() => FavoritesCubit(sl(), sl()));
 }
