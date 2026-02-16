822c049582829a2ce2f63a9f18432429b028b00f
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-16 13:13:04 +0800

    add favorites

--- lib/features/anime/domain/repositories/anime_repository.dart
+++ lib/features/anime/domain/repositories/anime_repository.dart
@@ -5,4 +5,9 @@ abstract class AnimeRepository {
   Future<(Failure?, List<Anime>?)> getTopAnime(int page);
   Future<(Failure?, List<Anime>?)> searchAnime(String query, int page);
   Future<(Failure?, Anime?)> getAnimeDetail(int id);
+
+  // Favorites
+  List<Anime> getFavorites();
+  Future<void> toggleFavorite(Anime anime);
+  bool isFavorite(int id);
 }
