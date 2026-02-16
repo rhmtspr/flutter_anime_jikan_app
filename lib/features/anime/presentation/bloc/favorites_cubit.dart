31b16070b392ecff494f85202815af29e6499521
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-16 15:59:23 +0800

    update favoritescubit

--- lib/features/anime/presentation/bloc/favorites_cubit.dart
+++ lib/features/anime/presentation/bloc/favorites_cubit.dart
@@ -1,22 +1,32 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import '../../../../core/usecases/usecase.dart';
 import '../../domain/entities/anime.dart';
 import '../../domain/usecases/get_favorites.dart';
 import '../../domain/usecases/toggle_favorite.dart';
 
-class FavoritesCubit extends Cubit<List<Anime>> {
-  final GetFavorites getFavorites;
-  final ToggleFavorite toggleFavoriteUseCase;
+class FavoritesCubit extends Cubit<Set<int>> {
+  FavoritesCubit() : super({});
 
-  FavoritesCubit(this.getFavorites, this.toggleFavoriteUseCase) : super([]);
-
-  Future<void> loadFavorites() async {
-    final data = await getFavorites(NoParams());
-    emit(data);
+  void loadFavorites() {
+    final favorites = Hive.box('favorites').values.cast<int>().toSet();
+    emit(favorites);
   }
 
-  Future<void> toggleFavorite(Anime anime) async {
-    await toggleFavoriteUseCase(anime);
-    await loadFavorites();
+  void toggleFavorite(int id) {
+    final box = Hive.box('favorites');
+    final newState = Set<int>.from(state);
+
+    if (newState.contains(id)) {
+      box.delete(id);
+      newState.remove(id);
+    } else {
+      box.put(id, id);
+      newState.add(id);
+    }
+
+    emit(newState);
   }
+
+  bool isFavorite(int id) => state.contains(id);
 }
