cea0d1c9b4d583acc608bb23b7029238ce2f491f
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-16 13:10:53 +0800

    create favorite_anime_model.g.dart

---
+++ lib/features/anime/data/models/favorite_anime_model.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'favorite_anime_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class FavoriteAnimeModelAdapter extends TypeAdapter<FavoriteAnimeModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  FavoriteAnimeModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return FavoriteAnimeModel(
+      id: fields[0] as int,
+      title: fields[1] as String,
+      imageUrl: fields[2] as String,
+      score: fields[3] as double,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, FavoriteAnimeModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.imageUrl)
+      ..writeByte(3)
+      ..write(obj.score);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is FavoriteAnimeModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
