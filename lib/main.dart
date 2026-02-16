994c396d5fddaa4499031bac35db4cc975a5c672
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-16 15:57:56 +0800

    add blocprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:anime_jikan_app/app.dart';
 import 'package:anime_jikan_app/core/di/injection.dart';
+import 'package:anime_jikan_app/features/anime/presentation/bloc/anime_list_bloc.dart';
+import 'package:anime_jikan_app/features/anime/presentation/bloc/favorites_cubit.dart';
+import 'package:anime_jikan_app/features/anime/presentation/bloc/search/search_bloc.dart';
+import 'package:anime_jikan_app/features/anime/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'features/anime/data/models/favorite_anime_model.dart';
 
@@ -17,5 +22,14 @@ Future<void> main() async {
 
   await initDependencies();
 
-  runApp(const AnimeExplorerApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => sl<AnimeListBloc>()),
+        BlocProvider(create: (_) => sl<SearchBloc>()),
+        BlocProvider(create: (_) => sl<FavoritesCubit>()..loadFavorites()),
+      ],
+      child: const AnimeExplorerApp(),
+    ),
+  );
 }
